6fe55dd98c07c25a57b189702c15cab93d0340db
Samu <t8masa00@students.oamk.fi>
2019-02-14 08:59:01 +0200

    valikkorakenne ja sivupohjat luotu

---
+++ add_books.php
@@ -0,0 +1,3 @@
+<?php include "menu.php"; ?>
+<h1>Lisää kirja</h1>
+<?php include "footer.php"; ?>
---
+++ menu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>kirjasto esimerkki</title>
+  </head>
+  <body>
+    <ul>
+      <li> <a href="show_books.php">Kirjalista</a></li>
+      <li> <a href="add_books.php">Lisää kirja</a></li>
+      <li> <a href="update_books.php">Muokkaa kirja</a></li>
+      <li> <a href="delete_books.php">Poista kirja</a></li>
+    </ul>
+    <hr>
